32fc7891e7a01e54c3b201a88081487ae1c2d792
ARUDOV <prosto_nickolos@mail.ru>
2026-03-03 17:38:33 +0300

    change game brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,36 +1,36 @@
-import prompt
 import random
+
+import prompt
 
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
   
-
-
     print('Answer "yes" if the number is even, otherwise answer "no".')
     current = 0
     while current < 3:
         number = random.randint(1, 100)
         print(f'Question: {number}')
         answer = prompt.string('Your answer: ')
-        current_answer = ''
+        cur_ans = ''
         if number % 2 == 0:
-            current_answer = 'yes'
+            cur_ans = 'yes'
         else: 
-            current_answer = 'no'
-        if answer == current_answer:
+            cur_ans = 'no'
+        if answer == cur_ans:
             current += 1
             print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{current_answer}'.\nLet's try again, {name}!")
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{cur_ans}'.")
+            print(f"Let's try again, {name}!")
             return
-    print(f'Congratulations, {name}')
+    print(f'Congratulations, {name}!')
+
 
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
-
 
 
 if __name__ == "__main__":
